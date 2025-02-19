9b292b483b21890fc9afae60144634942af99793
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2025-02-19 21:47:40 +0100

    chore(cleanup): heavily reduce example code complexity and comment density

--- _example/main.go
+++ _example/main.go
@@ -23,95 +23,28 @@ func main() {
 		log.Fatalf("Failed to parse URL: %v", err)
 	}
 
-	basicExample(url)
-	detailedExample(url)
-}
-
-// This example demonstrates how to measure the latency of a WebSocket connection
-// with a single function call.
-func basicExample(url *url.URL) {
-	fmt.Print("> Running basic example\n")
-	// Measure with a text message
+	// Measure latency with one of the convenience functions
 	var msg = "Hello, WebSocket!"
 	result, p, err := wsstat.MeasureLatency(url, msg, http.Header{})
 	if err != nil {
 		log.Fatalf("Failed to measure latency: %v", err)
 	}
-	fmt.Printf("Response: %s\n\n", p)
-	// Measure with a JSON message
-	/* var msg = map[string]interface{}{"json": "message", "compatible": "with", "your": "target", "ws": "server"}
-	result, p, _ := wsstat.MeasureLatencyJSON(url, msg)
-	fmt.Printf("Response: %s\n\n", p) */
-	// Measure with a ping message
-	//result, _ := wsstat.MeasureLatencyPing(url)
-	fmt.Printf("%+v\n", result)
-}
+	fmt.Printf("Basic example\nResponse: %s\n\nResult:\n%+v\n", p, result)
 
-// This example demonstrates how to measure the latency of a WebSocket connection
-// with more control over the steps in the process.
-func detailedExample(url *url.URL) {
-	fmt.Print("> Running detailed example\n")
-	var err error
-
-	// 1.a. Create a new WSStat instance
+	// Measure latency with more control over the steps in the process by using the WSStat instance
 	ws := wsstat.New()
 	defer ws.Close()
-	// 1.b. Load custom CA certificate if required
-	/* caCert, err := ioutil.ReadFile("path/to/ca.cert")
-	if err != nil {
-		log.Fatal(err)
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-	// Set up custom TLS configuration
-	tlsConfig := &tls.Config{
-		RootCAs: caCertPool,
-		// Further configuration as needed...
-	}
-	wsstat.SetCustomTLSConfig(tlsConfig) */
 
-	// 2. Establish a WebSocket connection
-	// This triggers the DNS lookup, TCP connection, TLS handshake, and WebSocket handshake timers
 	if err := ws.Dial(url, http.Header{}); err != nil {
 		log.Fatalf("Failed to establish WebSocket connection: %v", err)
 	}
 
-	// 3. Send a message and wait for the response
-	// This triggers the first message round trip timer
-	// 3.a. Write and read a message
-	msg := "Hello, WebSocket!"
 	ws.WriteMessage(websocket.TextMessage, []byte(msg))
-	msgType, p, err := ws.ReadMessage()
+	_, p, err = ws.ReadMessage()
 	if err != nil {
 		log.Fatalf("Failed to read message: %v", err)
 	}
-	fmt.Printf("Received message '%s' of type '%d'\n\n", p, msgType)
-	// 3.b. Send a custom message and wait for the response in a single function
-	/* msg := "Hello, WebSocket!"
-		p, err := ws.OneHitMessage(websocket.TextMessage, []byte(msg))
-	    if err != nil {
-	        log.Fatalf("Failed to send message: %v", err)
-	    }
-		fmt.Printf("Received message: %s\n\n", p) */
-	// 3.c. Send a JSON message and wait for the response
-	/* var msg = map[string]interface{}{"json": "message", "compatible": "with", "your": "target", "ws": "server"}
-		p, err := ws.OneHitMessageJSON(msg)
-	    if err != nil {
-	        log.Fatalf("Failed to send message: %v", err)
-	    }
-		log.Printf("Received message: %s", p) */
-	// 3.e. Alternatively send a ping
-	/* if err := ws.PingPong(); err != nil {
-		log.Fatalf("Failed to send ping: %v", err)
-	} */
 
-	// 4. Close the WSStat instance
-	// This triggers the total duration timer as well as closes the WebSocket connection
-	ws.Close()
-	if err != nil {
-		log.Fatalf("Failed to close WebSocket connection: %v", err)
-	}
-
-	// 5. Print the results
-	fmt.Printf("%+v\n", ws.Result)
+	result = ws.ExtractResult()
+	fmt.Printf("More involved example\nResponse: %s\n\nResult:\n%+v\n", p, result)
 }
